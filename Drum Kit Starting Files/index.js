0ff2d08595bd2b349f0a81979a8947ff296099c5
johanpq <johanp.queiroz@gmail.com>
2023-12-02 14:59:57 -0300

    feat: added switch statement

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,6 +5,34 @@ for(let i = 0; i < document.querySelectorAll('.drum').length; i++) {
 
         this.style.color = 'white';
 
+       var buttonclick = this.innerHTML;
+
+        switch(buttonclick) {
+            case 'w':
+                var audio = new Audio('sounds/tom-1.mp3').play();
+                break
+            case 'a':
+                var audio = new Audio('sounds/tom-2.mp3').play();
+                break
+            case 's':
+                var audio = new Audio('sounds/tom-3.mp3').play();
+                break
+            case 'd':
+                var audio = new Audio('sounds/tom-4.mp3').play();
+                break
+            case 'j':
+                var audio = new Audio('sounds/snare.mp3').play();
+                break
+            case 'k':
+                var audio = new Audio('sounds/crash.mp3').play();
+                break
+            case 'l':
+                var audio = new Audio('sounds/kick-bass.mp3').play();
+                break
+            default:
+                break
+        }
+
     });
 } 
 
