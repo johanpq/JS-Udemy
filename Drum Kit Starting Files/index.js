2fe1c625a4b98a44765be07f794d551ff22bbdde
johanpq <johanp.queiroz@gmail.com>
2023-12-02 15:06:29 -0300

    changed variables names

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -9,25 +9,25 @@ for(let i = 0; i < document.querySelectorAll('.drum').length; i++) {
 
         switch(buttonclick) {
             case 'w':
-                var audio = new Audio('sounds/tom-1.mp3').play();
+                var tom1 = new Audio('sounds/tom-1.mp3').play();
                 break
             case 'a':
-                var audio = new Audio('sounds/tom-2.mp3').play();
+                var tom2 = new Audio('sounds/tom-2.mp3').play();
                 break
             case 's':
-                var audio = new Audio('sounds/tom-3.mp3').play();
+                var tom3 = new Audio('sounds/tom-3.mp3').play();
                 break
             case 'd':
-                var audio = new Audio('sounds/tom-4.mp3').play();
+                var tom4 = new Audio('sounds/tom-4.mp3').play();
                 break
             case 'j':
-                var audio = new Audio('sounds/snare.mp3').play();
+                var snare = new Audio('sounds/snare.mp3').play();
                 break
             case 'k':
-                var audio = new Audio('sounds/crash.mp3').play();
+                var crash = new Audio('sounds/crash.mp3').play();
                 break
             case 'l':
-                var audio = new Audio('sounds/kick-bass.mp3').play();
+                var kickbass = new Audio('sounds/kick-bass.mp3').play();
                 break
             default:
                 console.log(buttonclick); //caso de algo errado
