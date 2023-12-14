34c6ed01aeac2d75cfd2aa308362e177c58156f5
johanpq <johanp.queiroz@gmail.com>
2023-12-14 12:25:47 -0300

    método setTimeout() aprendido

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,39 +5,15 @@ for(let i = 0; i < document.querySelectorAll('.drum').length; i++) {
 
         this.style.color = 'white';
 
-       var buttonclick = this.innerHTML;
+        var buttonclick = this.innerHTML;
 
-        switch(buttonclick) {
-            case 'w':
-                var tom1 = new Audio('sounds/tom-1.mp3').play();
-                break
-            case 'a':
-                var tom2 = new Audio('sounds/tom-2.mp3').play();
-                break
-            case 's':
-                var tom3 = new Audio('sounds/tom-3.mp3').play();
-                break
-            case 'd':
-                var tom4 = new Audio('sounds/tom-4.mp3').play();
-                break
-            case 'j':
-                var snare = new Audio('sounds/snare.mp3').play();
-                break
-            case 'k':
-                var crash = new Audio('sounds/crash.mp3').play();
-                break
-            case 'l':
-                var kickbass = new Audio('sounds/kick-bass.mp3').play();
-                break
-            default:
-                console.log(buttonclick); //caso de algo errado
-        }
+        MakeSound(buttonclick);
+        ButtonClickAnimation(buttonclick);
 
         // Pega os elementos que forem clicados. Switch pode fazer isso.
 
     });
-} 
-
+}
 // Vai pegar os 7 botões, poderia colocar o numero 7, ou pegar todos as tags com a classe ".drum" com o seu comprimento que é o que está.
 
 // ou até mesmo criando uma variavel, e pegando o comprimento dos botões para só colocar a variavel no parâmetro do loop
@@ -48,3 +24,48 @@ for(let i = 0; i < document.querySelectorAll('.drum').length; i++) {
 
 
 // Eu posso saber também ver, qual tag, elemento na tela, acionou a função que foi adicionada um evento para ouvir. E usando this eu eu sei. Se eu usar.: console.log(this); dentro da função e eu for checar o console depois de clicar no elemento(s), você verá qual tag foi clicada.
+
+document.addEventListener('keypress', function(event) {
+    MakeSound(event.key);
+    ButtonClickAnimation(event.key);
+});
+
+function MakeSound(key) {
+    switch(key) {
+        case 'w':
+            var tom1 = new Audio('sounds/tom-1.mp3').play();
+            break
+        case 'a':
+            var tom2 = new Audio('sounds/tom-2.mp3').play();
+            break
+        case 's':
+            var tom3 = new Audio('sounds/tom-3.mp3').play();
+            break
+        case 'd':
+            var tom4 = new Audio('sounds/tom-4.mp3').play();
+            break
+        case 'j':
+            var snare = new Audio('sounds/snare.mp3').play();
+            break
+        case 'k':
+            var crash = new Audio('sounds/crash.mp3').play();
+            break
+        case 'l':
+            var kickbass = new Audio('sounds/kick-bass.mp3').play();
+            break
+        default:
+            console.log(buttonclick); //caso de algo errado
+    }
+}
+
+function ButtonClickAnimation(CurrentKey) {
+    var activeButton = document.querySelector('.' + CurrentKey);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function() {
+        activeButton.classList.remove('pressed');
+    }, 100);
+
+    // setTimeout() é um método para fazer algo depois de um tempo determinado tempo.
+    // setTomeout(function, milliseconds, paramentro 1, paramentro 2);
+}
