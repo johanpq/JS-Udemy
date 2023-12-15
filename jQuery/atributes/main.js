320ab6461bd22899e147399cc005417ede683950
johanpq <johanp.queiroz@gmail.com>
2023-12-15 15:29:04 -0300

    aprendendo sobre atributos com jQuery

---
+++ jQuery/atributes/main.js
@@ -0,0 +1,11 @@
+/* CSS */
+
+$('h1').addClass("big-title position");
+
+$('button').addClass("button-color");
+
+// método de atributuo é attr()
+
+$('img').attr("src"); // pegando src = source da imagem.  Caso não exista, ele cria
+
+$('a').attr("href", "https://github.com/johanpq");  // trocando o link do href.
